aaf266240ae92b6da419ef9fd7b8cc09cbc9ac03
Reyn Tancio <reyn@gappify.com>
2024-10-04 15:34:33 +0800

    Added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "https://1mr3yn.github.io/marce-reyn/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
