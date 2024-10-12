91f7f4ad07afd8949e0e74efe3900c094a6f94ad
Reyn Tancio <reyn@gappify.com>
2024-10-12 08:30:20 +0800

    Updated gallery and footer

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t border-gray-300 py-4">
+    <footer className="border-t border-gray-300 pt-4 pb-8" style={{
+      background: 'linear-gradient(180deg,  #D1D5DB, #F3F4F6)'
+    }}>
       <div className="container mx-auto text-center">
-        <p className="mb-2 text-gray-800">© 2024 Marce & Reyn. L❤️ve is an adventure!</p>
+        <p className="mb-2 text-gray-800 italic">A Love Through Time: 8 Years and Beyond </p>
+        <p> © 2024 Marce ❤️ Reyn</p>
       </div>
     </footer>
   );
--- src/components/sections/gallery.jsx
+++ src/components/sections/gallery.jsx
@@ -23,17 +23,17 @@ const Gallery = () => {
 
   return (
     <section className="py-5 bg-gradient-to-b from-gray-300 to-gray-200">
-      <div className="px-1 mx-auto">
-        <div className="grid grid-cols-4 gap-1 sm:gap-1">
+      <div className="px-2 mx-auto">
+        <div className="grid grid-cols-3 md:grid-cols-4 gap-1">
           {images.map((image, index) => (
             <img
               key={index}
               src={image.src}
-              className="w-full h-25 md:h-60 lg:h-65 
-              object-cover rounded 
-              transition-transform 
+              className="w-full h-28 md:h-80
+              object-cover rounded-sm
+              transition-transform
               duration-200 
-              transform hover:scale-105"
+              transform hover:scale-110"
             />
           ))}
         </div>
